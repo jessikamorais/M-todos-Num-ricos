edd4f9791dcbf5eeba05dfecc459832d7cd4bc2f
jessikamorais <81242950+jessikamorais@users.noreply.github.com>
2021-06-18 18:47:38 -0300

    Add files via upload

---
+++ edo_rk4.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def rk4( f, x0, t ):
+
+    n = len( t )
+    x = np.array( [ x0 ] * n )
+    for i in range( n - 1 ):
+        h = t[i+1] - t[i]
+        k1 = h * f( x[i], t[i] )
+        k2 = h * f( x[i] + 0.5 * k1, t[i] + 0.5 * h )
+        k3 = h * f( x[i] + 0.5 * k2, t[i] + 0.5 * h )
+        k4 = h * f( x[i] + k3, t[i+1] )
+        x[i+1] = x[i] + ( k1 + 2.0 * ( k2 + k3 ) + k4 ) / 6.0
+
+    return x
